de07b6dab0688efaf23c0a4926794bca09f0e033
orangeztorm <taiwokenny45@gmail.com>
2025-10-05 14:03:20 +0100

    Add User, Email, and Password value objects with validation logic

---
+++ fast_api_project/services/auth-service/src/auth/domain/entities/entities.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field # generates useful methods like __init__, __repr__, etc.
+from datetime import datetime, timezone # python's built-in datetime module
+from typing import Optional # for optional type hinting
+
+
+@dataclass
+class User:
+    """
+    Represents a user in the authentication system.
+
+    LEARN:
+    - This class is part of the DOMAIN layer.
+    - It has no framework dependencies (pure Python).
+    - Business logic lives here, not in your controllers.
+    """
+
+    id: Optional[int]  # Unique identifier for the user, optional for new users
+    username: str  # Username of the user
+    email: str  # Email address of the user
+    hashed_password: str  # Hashed password for security
+    is_active: bool = True  # Indicates if the user account is active
+    created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))  # Timestamp of when the user was created
+
+    def deactivate(self):
+        '''
+        Deactivates the user account.
+        '''
+        self.is_active = False
+
+    def activate(self):
+        '''
+        Activates the user account.
+        '''
+        self.is_active = True
